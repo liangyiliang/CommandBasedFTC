c47b6496489574eaa9fda61234c89654e5543fda
liangyiliang <liangyiliang@oulook.com>
2020-01-11 14:53:07 -0500

    Added comments

--- src/org/commandftc/ParallelFirstCommand.java
+++ src/org/commandftc/ParallelFirstCommand.java
@@ -1,12 +1,17 @@
 package org.commandftc;
 
-
+/**
+ * A ParallelCommand that ends when either of the sub-commands end.
+ */
 public class ParallelFirstCommand extends ParallelCommand {
 
     public ParallelFirstCommand(Command ... commands) {
         super(commands);
     }
 
+    /**
+     * Overrides the whileUntilEither method for efficiency.
+     */
     @Override
     public ParallelFirstCommand whileUntilEither(Command other) {
         addCommand(other);
